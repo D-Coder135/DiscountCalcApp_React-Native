9285027f3185f06afbd94413c1ebce2fa6fdf167
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 19:54:54 +0530

    comment out the alignItems property for the const styles field

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'skyblue',
-    alignItems: 'center',
+    // alignItems: 'center',
     // justifyContent: 'center',
   },
 });
