69d25968f4bb2ad47551fdadbce364c62fa2429b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 20:05:35 +0530

    add properties for the inputText attribute inside the const styles field

--- App.js
+++ App.js
@@ -28,6 +28,6 @@ const styles = StyleSheet.create({
     marginHorizontal: 30
   },
   inputText: {
-    
+    backgroundColor: 'white', padding: 20, marginTop: 20, marginBottom: 20, borderRadius: 10, outline: 'none', textAlign: 'center', fontSize: 20
   }
 });
