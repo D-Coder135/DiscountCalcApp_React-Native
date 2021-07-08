b80676d05d89482f2e5e8b94d333675f26ada362
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 20:34:24 +0530

    add the internal styling for the View tag

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default class DiscountCalcApp extends React.Component {
         <TextInput style = {styles.inputText} />
       </View>
 
-      <View>
+      <View style = {styles.bottomButton}>
         <TouchableOpacity>
           Calculate
         </TouchableOpacity>
