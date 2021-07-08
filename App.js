3ed8ce5865474bca12f62d36b7f06e615f587840
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 21:10:41 +0530

    add some internal styling along with ternary condition inside it

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default class DiscountCalcApp extends React.Component {
           Calculate
         </TouchableOpacity>
 
-        <Text style = {{marginTop: 20}}>
+        <Text style = {{marginTop: 20, color: this.state.result ? 'green' : 'red'}}>
           Final Price After Discount: ₹ {this.state.amount - this.state.result}
         </Text>
 
