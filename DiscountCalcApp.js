6f595b3ddeb75922e9f8bb332c39efadfd871036
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 19:35:56 +0530

    add a class component along with the export default keyword

--- DiscountCalcApp.js
+++ DiscountCalcApp.js
@@ -2,7 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-
+export default class DiscountCalcApp extends React.Component {
+  
+}
 
 const styles = StyleSheet.create({
   container: {
